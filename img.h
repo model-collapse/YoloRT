973ece0589ecdd9c5e54999083786f4755a5713f
Charlie Yang <charlieyang@CH01207-Mbp.local>
2019-09-06 16:54:19 +0800

    yet another version no 255

--- img.h
+++ img.h
@@ -10,7 +10,7 @@ void mat_8u3c_to_darknet_blob(cv::Mat img, int32_t tensor_height, int32_t tensor
     for (int32_t c = 0; c < tensor_depth; c++) {
         for (int32_t y = 0; y < tensor_height; y++) {
             for (int32_t x = 0; x < tensor_width; x++) {
-                *p = ((float)img_float.at<cv::Vec3B>(y, x)[tensor_depth - c]) / 255.0;
+                *p = ((float)img_float.at<cv::Vec3B>(y, x)[tensor_depth - c]);
                 p++;
             }
         }
