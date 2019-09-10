337925ecdf61eb08928c4b76c157ca84a7aa7111
model-collapse <model-collapse@github.com>
2019-09-10 04:39:08 -0400

    try

--- plugin_try.cpp
+++ plugin_try.cpp
@@ -4,5 +4,16 @@
 
 int32_t main(int32_t argc, char** argv) {
     nvinfer1::IPluginV2* leakyRELU = createLReLUPlugin(0.1);
-    std::cerr << "serialized len = " << leakyRELU->getSerializationSize() << endl;
+    std::cerr << "serialized len = " << leakyRELU->getSerializationSize() << std::endl;
+    char* buf = new char[leakyRELU->getSerializationSize()];
+    leakyRELU->serialize(buf);
+    float* p = (float*)buf;
+    for (int32_t i = 0; i < 2; i++) {
+        std::cerr << "float@" << i << " = " << p[i] << std::endl;
+    };
+
+    int32_t* pp = (int32_t*)buf;
+    for (int32_t i = 0; i < 2; i++) {
+        std::cerr << "int@" << i << " = " << pp[i] << std::endl;
+    };
 }
