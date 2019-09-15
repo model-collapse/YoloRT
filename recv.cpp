7adbcbf10d85601e62c73e42fb35470906d26cca
Charlie Yang <charlieyang@CH01207-Mbp.local>
2019-09-15 21:21:44 +0800

    file list

--- recv.cpp
+++ recv.cpp
@@ -1,13 +1,36 @@
 #include "recv.h"
 #include <opencv2/opencv.hpp>
 #include <inttypes.h>
+#include "dirent.h"
 #define MAX_BUF_SIZE 1000000 // 1M buffer
+
+std::vector<std::string> list_dir(std::string path) {
+    DIR * dir;
+    struct dirent * ptr;
+    std::vector<std::string> ret;
+
+    dir = opendir((char *)path.c_str()); //打开一个目录
+    while((ptr = readdir(dir)) != NULL) //循环读取目录数据
+    {
+        std::string x = ptr->d_name;
+        std::string dir_path = path + std::string("/") + x;
+        ret.push_back(dir_path);
+    }
+    closedir(dir);//关闭目录指针
+
+    return ret;
+}
 
 ImageSource::ImageSource(const char* address, bool file_mode) 
     : ctx(1), buf(MAX_BUF_SIZE) {
     if (file_mode) {
-       
+        this->id = 0;
+        this->file_names = list_dir(address);
+        fprintf(stderr, "#%d files loaded!", this->files_names.size());
+        return;
     }
+
+    this->id = -1;
 
     try{
         this->socket = new zmq::socket_t(this->ctx, ZMQ_SUB);
@@ -33,6 +56,12 @@ ImageSource::~ImageSource() {
 }
 
 cv::Mat ImageSource::recv() {
+    if (this->id >= 0) {
+        std::string path = this->file_names[this->id];
+        this->id ++;
+        return cv::imread(path.c_str(), cv::IMREAD_COLOR);
+    }
+    
     fprintf(stderr, "receiving\n");
     fflush(stderr);
     this->socket->recv(&this->buf);
--- recv.h
+++ recv.h
@@ -14,8 +14,9 @@ private:
     zmq::context_t ctx;
     zmq::socket_t* socket;
     zmq::message_t buf;
-};
+
     int32_t id;
     std::vector<std::string> file_names;
+};
 
 #endif
