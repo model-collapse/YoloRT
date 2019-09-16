44c6cb023196d1e94f9ea50e26abbff232b4043d
Charlie Yang <charlieyang@CH01207-Mbp.local>
2019-09-16 14:37:50 +0800

    kafka

--- recv.cpp
+++ recv.cpp
@@ -107,6 +107,7 @@ ImageSourceKafka::ImageSourceKafka(const char* broker_addr, const char* group_na
     this->topic_name = topic_name;
 
     this->consumer->subscribe({this->topic_name});
+    this->consumer->set_timeout(30 * 1000);
 }
 
 ImageSourceKafka::~ImageSourceKafka() {
