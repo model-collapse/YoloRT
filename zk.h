3006791a479ad7bc52715bda240e6ce3e7885b26
Charlie Yang <charlieyang@CH01207-Mbp.local>
2019-09-20 17:24:17 +0800

    haha

--- zk.h
+++ zk.h
@@ -5,6 +5,6 @@
 
 int32_t init_zk(std::string addr);
 std::string zk_get(std::string path);
-int32_t update_kafka_settings(std::string zk_kafka_path, AllConfig* cfg) {
+int32_t update_kafka_settings(std::string zk_kafka_path, AllConfig* cfg);
 
 #endif
