6a9d897bc08a2204a6d379d08e98f29c84b5e435
Charlie Yang <charlieyang@CH01207-Mbp.local>
2019-10-25 15:42:29 +0800

    go make

--- api.h
+++ api.h
@@ -10,26 +10,26 @@ typedef void* hd_people_detector_t;
 typedef void* cv_mat_ptr_t;
 typedef const char* pstr_t;
 
-struct activity_detector_config_t {
+typedef struct activity_detector_config_t {
     char* model_path;
     char* names_path;
     int32_t batch_size;
     float ext_scale;
-};
+} activity_detector_config_t;
 
-struct people_detector_config_t {
+typedef struct people_detector_config_t {
     char* model_path;
     int32_t batch_size;
     float cls_thres;
     float nms_thres;
-};
+} people_detector_config_t;
 
-struct context_t {
+typedef struct context_t {
     cv_mat_ptr_t img;
     NvDsInferObjectDetectionInfo boxes[MAX_OBJ_CNT];
     int32_t num_boxes;
     pstr_t activities[MAX_OBJ_CNT][NUM_ACTIVITIES];
-};
+} context_t;
 
 #ifdef __cplusplus
 extern "C"{
@@ -37,18 +37,18 @@ extern "C"{
 
 struct context_t* new_context();
 
-int32_t free_ctx(struct context_t* c);
-int32_t free_adc(struct activity_detector_config_t c);
-int32_t free_pdc(struct people_detector_config_t c);
+int32_t free_ctx(context_t* c);
+int32_t free_adc(activity_detector_config_t c);
+int32_t free_pdc(people_detector_config_t c);
 
-hd_activity_detector_t new_activity_detector(struct activity_detector_config_t c);
-hd_people_detector_t new_people_detector(struct activity_detector_config_t c);
+hd_activity_detector_t new_activity_detector(activity_detector_config_t c);
+hd_people_detector_t new_people_detector(activity_detector_config_t c);
 
 int32_t free_ad(hd_activity_detector_t d);
 int32_t free_pd(hd_people_detector_t d);
 
-int32_t infer_ad(hd_activity_detector_t ad, struct context_t* c);
-int32_t infer_pd(hd_people_detector_t ad, struct context_t* c);
+int32_t infer_ad(hd_activity_detector_t ad, context_t* c);
+int32_t infer_pd(hd_people_detector_t ad, context_t* c);
 
 cv_mat_ptr_t imdecode(void* buf, int32_t size);
 int32_t free_cvmat(cv_mat_ptr_t a);
